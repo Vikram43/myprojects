933ca098805f8780517e324b443d9aea002262b1
Vikram43 <48677477+Vikram43@users.noreply.github.com>
2019-03-21 19:20:45 +0530

    Create create_report.sql

---
+++ create_report.sql
@@ -0,0 +1,20 @@
+
+PROMPT Creating report of all employee who has salary greater than or equal to 50000
+
+SET PAGESIZE 0
+SET LINESIZE 50
+SET HEADING OFF
+
+SPOOL D:\emp_report.csv
+
+SELECT '"Id","Name","Date of joining","Salary"'
+  FROM dual
+UNION ALL
+SELECT emp_id ||','||emp_name||','||TO_CHAR(date_join,'DD/MM/YYYY') ||','||sal
+  FROM (SELECT emp_id,emp_name,date_join,sal
+          FROM emp
+         WHERE sal >= 50000
+      ORDER BY emp_id);
+  
+SPOOL OFF 
+
